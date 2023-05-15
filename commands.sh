e9140d7fd937a34238f88cba14ad09cccb428010
Developer Writings <developerwritings@gmail.com>
2023-05-15 13:11:26 +0530

    Linux Commands

---
+++ commands.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+pwd
+whoami
+ip a | ip addr 
+touch sample.txt sample3.txt
+mkdir sample
+cp sample3.txt sample/sample3.txt
+cat sample.txt 
+head sample.txt
+tail sample.txt
+mv sample/sample.txt sample5
